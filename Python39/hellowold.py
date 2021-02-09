96089df4c134691e4945a2879f472248b46b9a04
okayasuuuuuu <int108036_1@icloud.com>
2021-02-09 14:23:31 +0900

    Python39初回コミット

---
+++ Python39/hellowold.py
@@ -0,0 +1,25 @@
+# for i in range(100):
+
+# print("こんにちは")
+
+int_x = input("Enter x:")
+int_y = input("Enter y:")
+print(int_x % int_y)
+
+print\
+("""これは、とても、とても
+
+
+長いコードです""")
+
+"""
+
+age = input("Enter your age:")
+int_age = int(age)
+if int_age < 21:
+    print("You are young!")
+elif int_age > 80:
+    print("You are Legend!")
+else:
+    print("Wow, you are old!")
+"""
